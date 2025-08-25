9705830298076c20e6b035b9190048c4c8126e35
野元駿介 <nomotoshunsuke@nomotoshunsukenoMacBook-Air.local>
2025-08-26 00:17:57 +0900

    add __routes and __version debug endpoints

--- app.py
+++ app.py
@@ -130,3 +130,7 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
 							
 	
+
+@app.route("/__version", methods=["GET"])
+def __version():
+    return os.environ.get("GIT_SHA","unknown"), 200
